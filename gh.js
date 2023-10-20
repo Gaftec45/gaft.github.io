87e9b733b9baafbacd71f9abc2304bc9cb7e95e5
Gaftec45 <146769388+Gaftec45@users.noreply.github.com>
2023-10-20 08:53:31 -0400

    Add files via upload

---
+++ gh.js
@@ -0,0 +1,62 @@
+const patientForm = document.getElementById("patientForm");
+const patientTableBody = document.getElementById("patientTableBody");
+const submitButton = document.getElementById("submitButton");
+const updateButton = document.getElementById("updateButton");
+let currentlyEditing = null;
+
+patientForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const firstName = document.getElementById("firstName").value;
+  const lastName = document.getElementById("lastName").value;
+  const email = document.getElementById("email").value;
+  const gender = document.getElementById("gender").value;
+  const recentlyInNigeria = document.getElementById("recentlyInNigeria").checked;
+
+  if (currentlyEditing === null) {
+    // Add a new record
+    const newRow = document.createElement("tr");
+    newRow.innerHTML = `
+      <td>${firstName}</td>
+      <td>${lastName}</td>
+      <td>${email}</td>
+      <td>${gender}</td>
+      <td>${recentlyInNigeria ? "Yes" : "No"}</td>
+      <td>
+        <button class="edit-btn">Edit</button>
+        <button class="delete-btn">Delete</button>
+      </td>
+    `;
+    patientTableBody.appendChild(newRow);
+  } else {
+    // Update an existing record
+    currentlyEditing.cells[0].textContent = firstName;
+    currentlyEditing.cells[1].textContent = lastName;
+    currentlyEditing.cells[2].textContent = email;
+    currentlyEditing.cells[3].textContent = gender;
+    currentlyEditing.cells[4].textContent = recentlyInNigeria ? "Yes" : "No";
+    updateButton.style.display = "none";
+    submitButton.style.display = "block";
+    currentlyEditing = null;
+  }
+
+  patientForm.reset();
+});
+
+patientTableBody.addEventListener("click", function (e) {
+  if (e.target.classList.contains("edit-btn")) {
+    const row = e.target.parentNode.parentNode;
+    currentlyEditing = row;
+    document.getElementById("firstName").value = row.cells[0].textContent;
+    document.getElementById("lastName").value = row.cells[1].textContent;
+    document.getElementById("email").value = row.cells[2].textContent;
+    document.getElementById("gender").value = row.cells[3].textContent;
+    document.getElementById("recentlyInNigeria").checked =
+      row.cells[4].textContent === "Yes";
+    updateButton.style.display = "block";
+    submitButton.style.display = "none";
+  } else if (e.target.classList.contains("delete-btn")) {
+    const row = e.target.parentNode.parentNode;
+    patientTableBody.removeChild(row);
+  }
+});
